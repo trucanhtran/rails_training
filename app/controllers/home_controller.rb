5b8f2386b3669954cb3d7d04a27665eca1c38970
trucanh <anhtructran111@gmail.com>
2021-02-27 22:18:53 +0700

    controllers and views

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+    def index
+    end
+
+    def contact
+    end
+
+    def profile
+    end
+
+    def contact123
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  get '/contact', to: 'home#contact'
+  get '/profile123', to:'home#profile'
+  get '/contact123', to:'home#contact123'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
