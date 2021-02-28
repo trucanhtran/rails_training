4320149885002eea372bc4297bd81f0129abea9c
trucanh <anhtructran111@gmail.com>
2021-02-28 22:25:56 +0700

    review, routes path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,20 @@ class HomeController < ApplicationController
     end
 
     def contact
+      @contact_id = params[:contact_id]
+      @contact1_id = params[:contact1_id]
+      @contact2_id = params[:contact2_id]
     end
 
     def profile
+      @profile_id = params[:profile_id]
     end
 
     def contact123
+      @contact_id = params[:contact_id]
+    end
+
+    def test
+       @test = params[:id]
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get '/contact', to: 'home#contact'
-  get '/profile123', to:'home#profile'
-  get '/contact123', to:'home#contact123'
+  get '/contact/:contact_id/:contact1_id/:contact2_id', to: 'home#contact'
+  get '/profile123/:id', to:'home#profile'
+  get '/contact123/:contact_id', to:'home#contact123'
+  get '/rails/:id/ruby', to:'home#test', as: "rails_ta"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
