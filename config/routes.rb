9c1b75b3cdea4a9008f61294a01518cbc7bd8c77
trucanh <anhtructran111@gmail.com>
2021-04-01 11:26:59 +0700

    add new Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :documents
   root to: "employers#index"
 
   resources :employers do
---
+++ db/migrate/20210401042221_create_documents.rb
@@ -0,0 +1,10 @@
+class CreateDocuments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :documents do |t|
+      t.string :name
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
