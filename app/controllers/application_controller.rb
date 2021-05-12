c7d95833ce866d0f9e23ad344051529a8f711f12
trucanh <anhtructran111@gmail.com>
2021-05-12 13:49:22 +0700

    catch all exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, :with => :error_render_method
+
+  def error_render_method
+    respond_to do |type|
+      type.xml { render :template => "errors/error_404", :status => 404 }
+      type.all  { render :nothing => true, :status => 404 }
+    end
+    true
+
+  end
 end
