b7ea6f65c3be8ce28149391cfe85ef2aa8ae449c
trucanh <anhtructran111@gmail.com>
2021-05-31 15:12:49 +0700

    add jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,22 +15,29 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-global.$ = require('jquery')
+require("jquery");
 
-$(document).on("onkeyup", "#id_keyword", function () {
-  $.ajax({
-    url: "/search",
-    type: "POST",
-    dataType: "json",
-    data: {
-      aaa: $("#id_keyword").value,
-      authenticity_token: window._token,
-    },
-    success: function (data) {
-      debugger;
-    },
-    error: function (data) {
-      debugger;
-    },
+function appendDataProduct(data) {
+  document.getElementById("id_books_list").innerHTML = "";
+  for(var i = 0; i< data.length; i++) {
+    var node = document.createElement("div");
+    var textNode = document.createTextNode(data[i].name);
+    node.appendChild(textNode);
+    document.getElementById("id_books_list").appendChild(node);
+  }
+}
+
+// bat dau goi jquery, phai dat code goi jquery trong day
+//https://www.w3schools.com/jquery/jquery_intro.asp
+$(document).ready(function(){
+  // goi su kien onkeyup tac dong len the input co id la id_keyword
+  // bien event tham khao den the input, dung event lay ra gia tri value cua the input
+  $(document).on("keyup", "#id_keyword", function(event) {
+    const keyword = event.target.value;
+    // dung ham $.post cua jquery de goi ajax, hai bien data la mot mang gia tri tra ve tu server
+    // bien status la trang thai tra ve, neu tra ve that bai thi la false, con true la success
+    $.post("/search", {aaa: keyword}, function(data, status) {
+      appendDataProduct(data);
+    });
   });
 });
